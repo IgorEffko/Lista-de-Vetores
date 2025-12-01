2f6b6d63fba1a8b0c219f9353c9482c6f72c0d4f
IgorEffko <igor.vendruscolo@escola.pr.gov.br>
2025-12-01 08:47:44 -0300

    fiz a estrtura base das funções

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-function adicionar() {
 
+let vetor = [];
+console.log(vetor);
+
+
+function adicionar() {
+    let valor = getInput();
+    console.log(valor);
+    addvetor(valor); 
+    console.log(vetor); 
+    imprimir();  
+}
+
+function getInput() {
+    return document.getElementById("input").value;
+}
+
+function addvetor(valor) {
+    vetor[0] = valor;
+    
 }
 
 function imprimir() {
-    
+    document.getElementById("input").value = vetor[0];
 }
